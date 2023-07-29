fa1c5d10d752f4514deb252a0910168662c3acc9
Nafis Saami Azad <nafissazad@gmail.com>
2023-07-29 17:53:00 +0600

    Implemented CRUD operations

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+//        dd($request->all());
+        $request->validate([
+            'name'=>'required',
+            'slug'=>'required',
+            'price'=>'required'
+        ]);
+//        dd($request->all());
+        return Product::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Product::find($id);
+    }
+
+    public function search(string $name)
+    {
+        return Product::where('name','like', '%'.$name.'%')->get();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $product = Product::find($id);
+        $product->update($request->all());
+
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        return Product::destroy($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', function (){
-    return "products";
-});
+Route::apiresource('products', ProductController::class);
+Route::get('/products/search/{search}', [ProductController::class, 'search']);
+
+//Route::get('/products', [ProductController::class, 'index']);
+//
+//Route::post('/products', [ProductController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
